82a0dd419b4c1d23f5ce00d916ec2c08ccbebae4
karl <elrion018@gmail.com>
2022-07-22 19:43:19 +0900

    Refactor : .gitignore 생성, 의존성 정리, HMR 세팅

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,27 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  // enntry file
-  // entry: ["@babel/polyfill", "./src/js/write.js"],
   entry: {
-    main: ["@babel/polyfill", "./src/js/main.js"],
-    // board: ["@babel/polyfill", "./src/js/board.js"],
+    main: ["./src/js/main.js"],
   },
   // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
   output: {
-    path: path.resolve(__dirname, "src/dist"),
+    path: path.resolve(__dirname, "dist"),
     filename: "[name].js",
+    clean: true,
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: [path.resolve(__dirname, "src/js")],
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            plugins: [
-              "@babel/plugin-proposal-class-properties",
-              "@babel/plugin-transform-arrow-functions",
-            ],
-          },
-        },
-      },
-    ],
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Tetris",
+    }),
+  ],
+
+  devServer: {
+    compress: true,
+    port: 3000,
   },
-  devtool: "source-map",
-  // https://webpack.js.org/concepts/mode/#mode-development
+
   mode: "development",
 };
