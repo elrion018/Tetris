62a52611598a1e11c66bdebe010a658e7b8cdc5a
karl <elrion018@gmail.com>
2022-07-22 20:58:01 +0900

    Chore : tsconfig, ts-loader 등 타입스크립트 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,37 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: ["./src/js/main.js"],
+    main: ['./src/js/main.js'],
   },
   // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
     clean: true,
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Tetris",
+      title: 'Tetris',
     }),
   ],
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/i,
+        loader: 'ts-loader',
+        exclude: ['/node_modules'],
+      },
+    ],
+  },
 
   devServer: {
     compress: true,
     port: 3000,
   },
 
-  mode: "development",
+  mode: 'development',
 };
