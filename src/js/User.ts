5c27b7a469585dbed420a635bc7d2b0c4f3b0861
karl <elrion018@gmail.com>
2022-07-23 13:40:48 +0900

    Refactor : updateUserInfo 메소드와 검증 메소드 추가

--- src/js/User.ts
+++ src/js/User.ts
@@ -4,22 +4,33 @@ export interface UserInfo {
   level: number;
 }
 
+const initialUserInfo = { score: 0, lines: 0, level: 1 };
+
 export class User {
   userInfo: UserInfo;
 
   constructor() {
-    this.userInfo = { score: 0, lines: 0, level: 1 };
+    this.userInfo = initialUserInfo;
   }
 
   reset() {
-    this.userInfo = { score: 0, lines: 0, level: 1 };
+    this.userInfo = initialUserInfo;
   }
 
   getUserInfo() {
     return { ...this.userInfo };
   }
 
-  setUserInfo(userInfo: UserInfo) {
-    this.userInfo = userInfo;
+  setUserInfo(newUserInfo: UserInfo) {
+    this.userInfo = newUserInfo;
+  }
+
+  updateUserInfo(newUserInfo: UserInfo) {
+    if (this.newUserInfoIsSameWithUserInfo(newUserInfo))
+      this.setUserInfo(newUserInfo);
+  }
+
+  newUserInfoIsSameWithUserInfo(newUserInfo: UserInfo) {
+    return JSON.stringify(this.userInfo) === JSON.stringify(newUserInfo);
   }
 }
